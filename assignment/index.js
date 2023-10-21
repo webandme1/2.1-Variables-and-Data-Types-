39ad5e56594d76a43c32676ac71e5b1dc4cde537
webandme1 <94754776+webandme1@users.noreply.github.com>
2023-10-20 17:01:32 -0700

    updated

--- assignment/index.js
+++ assignment/index.js
@@ -1,15 +1,20 @@
 let studentFirstName = "Brandon";
-let studentLastName = "N";
+let studentLastName = "Newman";
 let grade1 = 53;
 let grade2 = 96;
 let grade3 = 65;
 let grade4 = 74;
 let passingGrade = 80;
+let averageGrade1 = "has an average grade of";
+let passingQuestion= "Did the student pass?"
+let averageGrade=((grade1+grade2+grade3+grade4)/4);
+let Pass=averageGrade>passingGrade;
+console.log ((grade1+grade2+grade3+grade4)/4) 
 //1.Create a variable called average that computes for the average from grade1 to grade4
-
+console.log (studentLastName)
 //2. Change the value of studentLastName to "Newman" 
-
+console.log ((studentFirstName)+(" ")+(studentLastName)+(averageGrade1)+" "+(averageGrade))
 //3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
 //ex. Jane Doe has an average grade of 100.
-
+console.log ((passingQuestion)+" "+Pass)
 //4. Write code that would answer this question: Did the student pass?
